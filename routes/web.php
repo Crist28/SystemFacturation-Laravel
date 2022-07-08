78f35643db9f2f65c897b9340ab0b7c4a3db983d
Cristian Jaimes <cristianjaimes691@gmail.com>
2022-07-08 10:20:46 -0500

    Cambios en el sistema de facturacion

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\BuyerController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\InvoiceDetailController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,12 +21,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+
+Route::middleware('auth')->group(function () {
+    Route::resource('/products', ProductController::class);
+    
+    Route::resource('/buyers', BuyerController::class);
+    
+    Route::resource('/invoices', InvoiceController::class);
+    
+    Route::post('/invoices/complete/{invoice}', [InvoiceController::class, 'completeSend'])->name('invoices.complete');
+
+    Route::resource('/invoice-details', InvoiceDetailController::class);
+    
+    Route::get('/invoices/add-product/{invoice}/', [InvoiceDetailController::class, 'create'])->name('invoices.add_products');
+
 });
